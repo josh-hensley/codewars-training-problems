7bf17ec7c2ca22bf8f392199cc88126165828891
josh-hensley <me@joshhensley.com>
2025-03-14 21:56:04 -0500

    started messing with chrome extensions, did not finish

---
+++ index.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('create-btn');
+
+
+const getInfo = async () => {
+    const [ tab ] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const kataId = tab.url.split('/')[4];
+    const language = tab.url.split('/')[6];
+    const extension = language == 'javascript' ? 'js' :
+        language == 'python' ? 'py' :
+        language == 'typescript' ? 'ts' : 'txt';
+    
+    const response = await fetch(`https://www.codewars.com/api/v1/code-challenges/${kataId}`);
+    const { name, slug, description } = await response.json();
+    const md = `
+# ${name}
+
+${description}
+`
+    const page = await chrome.webRequest.onCompleted(tab.id, {greeting: "hello"});
+    console.log(page);
+    // await fs.write(`${language}/${slug}/README.md`, md, 'utf-8', err => err ? console.error(err) : console.log('Success!'))
+    // await fs.write(`${language}/${slug}/solution.${extension}`, solution, 'utf-8', err => err ? console.error(err) : console.log('Success!'))
+}
+button.addEventListener('click', getInfo);
